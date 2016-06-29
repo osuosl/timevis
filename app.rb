c416ccf8905e7abbc9fedf080eebaa385f889428
iCHAIT <cgupta319@gmail.com>
2016-06-29 23:41:20 +0530

    defined endpoints for get_*

--- app.rb
+++ app.rb
@@ -1,5 +1,25 @@
 require 'sinatra'
 
 get '/' do
-  erb :login
+  erb :login, :layout => false
+end
+
+get '/home' do
+  erb :home
+end
+
+get '/activities' do
+  erb :activities
+end
+
+get '/projects' do
+  erb :projects
+end
+
+get '/times' do
+  erb :times
+end
+
+get '/users' do
+  erb :users
 end
