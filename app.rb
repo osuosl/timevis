72bec720e246ebc947f38a8882caee672e8b0991
iCHAIT <cgupta319@gmail.com>
2016-07-02 23:47:17 +0530

    static files for nvd3,chart with dummy data working

--- app.rb
+++ app.rb
@@ -23,3 +23,29 @@ end
 get '/users' do
   erb :users
 end
+
+get '/projVsHours' do
+  # "Project vs Hours Worked"
+  # erb :chart1, :layout => false
+  erb :chart1
+end
+
+get '/lifeCycle' do
+  "Time Usage vs Life cycle of a project over months/years."
+end
+
+get '/UsersVsHours' do
+  "Users vs Hours Worked on weekly/monthly basis"
+end
+
+get '/ActivityVAr' do
+  "Activity variation for a user over a year"
+end
+
+get '/ProjVsTeam' do
+  'All Projects vs Team Size'
+end
+
+get '/activityVsTime' do
+  'Activities vs Time Spent by org. on each project.'
+end
